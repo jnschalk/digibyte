471dcc757d4c0f614b1ffc1f9f417fd1542d09bf
GTO90 <gto90@protonmail.com>
2021-07-23 23:33:10 -0500

    MERGE RESOLVE 30: src/txmempool.cpp

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -526,15 +526,9 @@ void CTxMemPool::removeForReorg(CChainState& active_chainstate, int flags)
                 indexed_transaction_set::const_iterator it2 = mapTx.find(txin.prevout.hash);
                 if (it2 != mapTx.end())
                     continue;
-<<<<<<< HEAD
-                const Coin &coin = pcoins->AccessCoin(txin.prevout);
-                if (nCheckFrequency != 0) assert(!coin.IsSpent());
-                if (coin.nHeight < Params().GetConsensus().multiAlgoDiffChangeTarget) {                    
-=======
                 const Coin &coin = active_chainstate.CoinsTip().AccessCoin(txin.prevout);
                 if (m_check_ratio != 0) assert(!coin.IsSpent());
                 unsigned int nMemPoolHeight = active_chainstate.m_chain.Tip()->nHeight + 1;
->>>>>>> bitcoin/8.22.0
                 if (coin.IsSpent() || (coin.IsCoinBase() && ((signed long)nMemPoolHeight) - coin.nHeight < COINBASE_MATURITY)) {
                     txToRemove.insert(it);
                     break;
