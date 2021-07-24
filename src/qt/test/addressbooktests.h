b20e53f751cddce37cae8683790e58965fd66f57
GTO90 <gto90@protonmail.com>
2021-07-23 22:38:03 -0500

    MERGE RESOLVE 23: src/qt/test/addressbooktests.h

--- src/qt/test/addressbooktests.h
+++ src/qt/test/addressbooktests.h
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-=======
-// Copyright (c) 2018-2020 The DigiByte Core developers
+// Copyright (c) 2018-2020 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
->>>>>>> bitcoin/8.22.0
 #ifndef DIGIBYTE_QT_TEST_ADDRESSBOOKTESTS_H
 #define DIGIBYTE_QT_TEST_ADDRESSBOOKTESTS_H
 
